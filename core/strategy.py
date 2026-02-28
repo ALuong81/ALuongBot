081067d178d88f1b9b7f98f275e354c8dcd520cc
ALuong81 <anhln81@gmail.com>
2026-03-01 01:06:59 +0700

    Update strategy.py

--- core/strategy.py
+++ core/strategy.py
@@ -1,1 +1,15 @@
+from core.factors import add_indicators
 
+def filter_stock(symbol, df):
+    df = add_indicators(df)
+
+    latest = df.iloc[-1]
+
+    cond1 = latest["close"] > latest["MA20"]
+    cond2 = latest["MA20"] > latest["MA50"]
+    cond3 = latest["MA50"] > latest["MA200"]
+
+    if cond1 and cond2 and cond3:
+        return True, latest["close"]
+
+    return False, latest["close"]
