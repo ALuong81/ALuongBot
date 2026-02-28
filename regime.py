1d43d7ef5f5e2c9c8442ee78340deedfc4f2cdee
ALuong81 <anhln81@gmail.com>
2026-03-01 01:01:03 +0700

    Delete regime.py

--- regime.py
+++
@@ -1,7 +0,0 @@
-def detect_regime(index_df):
-    ma200 = index_df["close"].rolling(200).mean()
-    price = index_df["close"].iloc[-1]
-
-    if price > ma200.iloc[-1]:
-        return "BULL"
-    return "BEAR"
