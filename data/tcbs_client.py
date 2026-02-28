d7672c0dfc71ea11a1fa9c45a298c3ed532b4fec
ALuong81 <anhln81@gmail.com>
2026-03-01 01:26:13 +0700

    Update tcbs_client.py

--- data/tcbs_client.py
+++ data/tcbs_client.py
@@ -4,6 +4,12 @@ from datetime import datetime, timedelta
 
 BASE_URL = "https://apipubaws.tcbs.com.vn/stock-insight/v1/stock/bars"
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://stock.tcbs.com.vn/",
+    "Origin": "https://stock.tcbs.com.vn"
+}
+
 def get_price(symbol, days=200):
     end = datetime.today()
     start = end - timedelta(days=days)
@@ -16,12 +22,17 @@ def get_price(symbol, days=200):
         "to": int(end.timestamp())
     }
 
-    response = requests.get(BASE_URL, params=params)
+    response = requests.get(BASE_URL, params=params, headers=HEADERS)
 
     if response.status_code != 200:
+        print(response.text)
         raise Exception(f"Lỗi TCBS {symbol}")
 
     data = response.json()
+
+    # Nếu không có dữ liệu
+    if "t" not in data or len(data["t"]) == 0:
+        raise Exception(f"Không có dữ liệu {symbol}")
 
     df = pd.DataFrame({
         "time": data["t"],
