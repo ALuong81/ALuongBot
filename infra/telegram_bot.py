f6266362a157f570b10883c5aeba3dc27849d87c
ALuong81 <anhln81@gmail.com>
2026-03-01 03:26:45 +0700

    Update telegram_bot.py

--- infra/telegram_bot.py
+++ infra/telegram_bot.py
@@ -1,7 +1,21 @@
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-def send_photo(path):
+
+def send_message(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+    requests.post(url, data=payload)
+
+
+def send_photo(path: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendPhoto"
     with open(path, "rb") as f:
-        requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID}, files={"photo": f})
+        requests.post(
+            url,
+            data={"chat_id": TELEGRAM_CHAT_ID},
+            files={"photo": f}
+        )
