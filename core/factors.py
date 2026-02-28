9ce029c52f65f1b2ed03b60fd74a218c896f6fc5
ALuong81 <anhln81@gmail.com>
2026-03-01 01:06:36 +0700

    Update factors.py

--- core/factors.py
+++ core/factors.py
@@ -1,1 +1,5 @@
-
+def add_indicators(df):
+    df["MA20"] = df["close"].rolling(20).mean()
+    df["MA50"] = df["close"].rolling(50).mean()
+    df["MA200"] = df["close"].rolling(200).mean()
+    return df
