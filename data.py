bbf75fee254b1378526083a48d013a1470bd475c
ALuong81 <anhln81@gmail.com>
2026-02-28 22:42:22 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,25 +1,15 @@
+from vnstock import Vnstock
 import pandas as pd
-from vnstock import Quote
-from config import DATA_START
-
-def get_price(symbol, is_index=False):
-    try:
-        q = Quote(symbol=symbol, source="VCI")
-        df = q.history(start=DATA_START, interval="1D")
-
-        if df is None or df.empty:
-            return None
-
-        df.columns = [c.lower() for c in df.columns]
-        return df
-
-    except Exception as e:
-        print(f"Error fetching {symbol}: {e}")
-        return None
-
 
 def get_universe():
-    hose = ["HPG","FPT","MWG","VCB"]
-    hnx = ["PVS","SHS"]
-    upcom = ["ACV","BSR"]
-    return hose + hnx + upcom
+    # 3 sàn
+    return [
+        "VCB","BID","CTG","HPG","FPT","MWG","VNM",
+        "SSI","VND","GAS","PLX","PVD","HSG",
+        "REE","PNJ","DXG","NLG","KDH"
+    ]
+
+def get_price(symbol):
+    stock = Vnstock().stock(symbol=symbol, source="VCI")
+    df = stock.quote.history(start="2023-01-01", interval="1D")
+    return df
