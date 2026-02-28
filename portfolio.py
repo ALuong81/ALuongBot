ff524d48c4c5486685ffa18966001a62a4033deb
ALuong81 <anhln81@gmail.com>
2026-02-28 22:51:03 +0700

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,19 +1,14 @@
-from config import TARGET_VOL, TOP_N_BULL, TOP_N_BEAR
+def allocate(signals):
+    capital = 100000000  # 100 triệu
+    selected = list(signals.keys())
 
-def build_portfolio(scores, regime):
+    if not selected:
+        return {}
 
-    sorted_items = sorted(scores.items(), key=lambda x: x[1]["score"], reverse=True)
+    weight = capital / len(selected)
 
-    top_n = TOP_N_BULL if regime=="BULL" else TOP_N_BEAR
-    selected = sorted_items[:top_n]
-
-    portfolio = []
-    for sym, data in selected:
-        vol = data["vol"]
-        weight = TARGET_VOL / (vol + 1e-6)
-        portfolio.append((sym, data["score"], weight))
-
-    total_weight = sum([x[2] for x in portfolio])
-    portfolio = [(s, sc, w/total_weight) for s, sc, w in portfolio]
+    portfolio = {}
+    for s in selected:
+        portfolio[s] = weight
 
     return portfolio
